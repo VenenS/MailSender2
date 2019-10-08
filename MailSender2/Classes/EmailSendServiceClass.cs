cdd245615da77e16ee8a5b7d6bf0432d97b7db1d
Николай <semlya.kolyan@mail.ru>
2019-10-08 15:47:40 +0300

    Добавленна асинхронность приложения

--- MailSender2/Classes/EmailSendServiceClass.cs
+++ MailSender2/Classes/EmailSendServiceClass.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Mail;
 using System.Windows;
 using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace MailSender2.Classes
 {
@@ -58,11 +59,64 @@ namespace MailSender2.Classes
             }
         }
 
+        private async Task SendMailAsync(string mail, string name) // Отправка email конкретному адресату
+        {
+            using (MailMessage mm = new MailMessage(strLogin, mail))
+            {
+                mm.Subject = strSubject;
+                mm.Body = strBody + "\nПисьмо от " + DateTime.Now;
+                mm.IsBodyHtml = false;
+                SmtpClient sc = new SmtpClient(strSmtp, iSmtpPort);
+                sc.EnableSsl = true;
+                sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+                sc.UseDefaultCredentials = false;
+                sc.Credentials = new NetworkCredential(strLogin, strPassword);
+                try
+                {
+                    await sc.SendMailAsync(mm).ConfigureAwait(false);
+                    View.SendEndWindow sendEnd = new View.SendEndWindow();
+                    sendEnd.ShowDialog();
+                }
+                catch (Exception ex)
+                {
+                    View.ErorrWindow erorrWindow = new View.ErorrWindow();
+                    erorrWindow.ShowDialog();
+                    MessageBox.Show("Невозможно отправить письмо " + ex.ToString());
+                }
+            }
+        }
+
         public void SendMails(ObservableCollection<Email> emails)
         {
             foreach (Email email in emails)
             {
                 SendMail(email.Address, email.Name);
+            }
+        }
+
+        public void SendMailsParallel(ObservableCollection<Email> emails)
+        {
+            foreach(Email email in emails)
+            {
+                ThreadPool.QueueUserWorkItem(_ => SendMails(emails));
+            }
+        }
+
+        public async Task SendParallelAsync(ObservableCollection<Email> emails)
+        {
+            var send_task = new List<Task>();
+            foreach(Email email in emails)
+            {
+                send_task.Add(SendMailAsync(email.Address, email.Name));
+            }
+            await Task.WhenAll(send_task).ConfigureAwait(false);
+        }
+
+        public async Task SendMailsAsync(ObservableCollection<Email> emails)
+        {
+            foreach(Email email in emails)
+            {
+                await SendMailAsync(email.Address, email.Name).ConfigureAwait(false);
             }
         }
     }
